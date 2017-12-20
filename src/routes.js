b485a0b804cd5907d35c5fbb4817c90f0d6379c6
Jim <jim@designbyjim.co.uk>
2017-12-20 15:11:47 +0000

    GA

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import { Router, IndexRoute, Route } from 'react-router';
 
+import ReactGA from 'react-ga';
+
+
 import App from './App';
 import List from './pages/List';
 import SinglePost from './pages/Single';
-import SinglePage from './pages/Single';
+// import SinglePage from './pages/Single';
 import NotFound from './components/NotFound';
+
+ReactGA.initialize('UA-1360554-9');
 
 const Routes = (props) => (
   <Router {...props}>
     <Route path="/" component={App}>
       <IndexRoute component={List} />
+      <Route path="/:slug" component={SinglePost} />
       <Route path="/page/:page" component={List} />
       <Route path="/:cat/:slug" component={SinglePost} />
       <Route path="/:cat/page/:page" component={List} />
