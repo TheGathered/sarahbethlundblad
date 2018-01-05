7f3fd2186128a49ce57a3c8d2472e954e24efde1
Jim <jim@designbyjim.co.uk>
2018-01-05 16:08:16 +0000

    loader animation

---
+++ src/components/Loading.js
@@ -0,0 +1,10 @@
+// components/NotFound.js
+import React from 'react';
+import './Loader.css';
+
+const Loading = (...props) => {
+  return (<div className="loader" />)
+}
+
+
+export default Loading;
