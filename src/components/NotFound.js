5567c100da6eacea8678b82a90a6fade3652e573
Jim <jim@designbyjim.co.uk>
2018-01-05 16:09:09 +0000

    Notfound

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,10 +1,20 @@
 // components/NotFound.js
 import React from 'react';
+import {Helmet} from "react-helmet";
 
-const NotFound = () =>
-  <div>
-    <h3>404 page not found</h3>
-    <p>We are sorry but the page you are looking for does not exist.</p>
-  </div>
+const NotFound = (...props) =>
+
+	<div className="App">
+		<Helmet >
+				<meta charSet="utf-8" />
+				<title>404 page not found</title>
+				<link rel="next" href="/" />
+		</Helmet>
+					<div>
+						<h1>404: page not found</h1>
+						<p>We are sorry but the page you are looking for does not exist.</p>
+					</div>
+	</div>
+
 
 export default NotFound;
